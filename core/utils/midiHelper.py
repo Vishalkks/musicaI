81eb5e97d6481a5daa925ffe6ce749e40688f056
Vishalkks <vishalkks@gmail.com>
2017-11-25 19:20:37 +0530

    put everything in one file

---
+++ core/utils/midiHelper.py
@@ -0,0 +1,14 @@
+import pygame
+import pygame.midi
+from pygame.locals import *
+
+def readMusic():
+    #infinite loop, give whole line of midi output
+    pass
+
+def convertMidiToNote(midi_val):
+    pass
+
+def outputSoundForMidi(midi_val):
+    pass
+
--- core/utils/parseGrammar.py
+++ core/utils/parseGrammar.py
@@ -1,11 +1,27 @@
 from os import listdir
 from pprint import pprint
-import pygame
+import pygame, random
 import pygame.midi
 from pygame.locals import *
-
+import time
 rules = {}
-
+NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+note2num = {
+'C':48,
+'D':50,
+'E':52,
+'F':53,
+'G':55,
+'A':57,
+'B':59,
+'C1':60,
+'D1':62,
+'E1':64,
+'F1':65,
+'G1':67,
+'A1':69,
+'B1':71
+}
 def populateRules():
     global rules
     files = [x for x in listdir("../rules") if '~' not in x]
@@ -25,25 +41,28 @@ def getInput():
     input_id = 3
     i = pygame.midi.Input( input_id )
     print pygame.midi.get_device_info(3)
+    output = pygame.midi.Output(2)
 
-    NOTES = ['C','C#','D','D#','E', 'F', 'F#', 'G', 'G#', 'A', 'A#','B']
     going = True
     while going:
-            events = event_get()
-            for e in events:
-                    if e.type in [QUIT]:
-                            going = False
-                    if e.type in [KEYDOWN]:
-                            going = False
-
             if i.poll():
                     midi_events = i.read(1)
-                    if midi_events[0][1] not 0:
-                        note = NOTES.index(midi_events[0][1]%12)
-                        vel = midi_events[0][2]
-                        chords = rules['C'][note]
-                        print ("Note: "+note+" Vel:"+vel+" chords found:"+str(chords))
-    del is
+                    if midi_events[0][0][2] != 0:
+                        print(midi_events)
+                        note = NOTES[midi_events[0][0][1]%12]
+                        vel = midi_events[0][0][2]
+                        try:
+                            chords = rules['C'][note]
+                            if len(chords)>1:
+                                chords.pop(random.randint(0,100)%2)
+                            random.shuffle(chords[0])
+                            for chordNote in chords[0]:
+                                output.note_on(note2num[chordNote], velocity=100)
+                                time.sleep(0.3)
+                                output.note_off(note2num[chordNote], velocity=0)
+                        except KeyError:
+                            chords = None
+                            print ("Note: "+note+" Vel:"+str(vel)+" chords found:"+str(chords))
 
 populateRules()
 
