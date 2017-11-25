2e10739ed4bfd67e38c7a1a5361c3521a7633dc3
varun <varun.sapre@gmail.com>
2017-11-26 02:48:44 +0530

    added timeInterval and output calls

--- core/inputProcess/inputProcess.py
+++ core/inputProcess/inputProcess.py
@@ -13,8 +13,8 @@ time_interval = None
 
 def initTempo():
 	global tempo, time_interval
-	tempo = int(raw_input("Enter desired tempo: "))
-	time_interval = float(1000*4*60/tempo)
+	tempo = 120#int(raw_input("Enter desired tempo: "))
+	time_interval = float(1000*60/tempo)
 
 
 def populateRules():
@@ -37,42 +37,30 @@ def getMidiInput():						#gets midi input, and converts to note
 	note = None
 	vel = None
 	chords = None
-	prev_bar_start = None
-
+	bar_start = None
+	next_bar = None
+	next_bar_count = None
+	hasStarted = False
+	prevChord = None
 	pygame.midi.init()
     input_id = 3
     i = pygame.midi.Input( input_id )
-    print pygame.midi.get_device_info(3)
-	first = False
-
-	while !first:
-        if i.poll():
-			midi_events = i.read(1)
-			print(midi_events)
-			if midi_events[0][0][2] != 0: 			#velocity!=0 [keyPressed]
-				first = True
-				prev_bar_start = midi_events[1]
-				note = NOTES[midi_events[0][0][1]%12]		#noteNum % 12
-			    vel = midi_events[0][0][2]
-				try:
-			        chords = rules['C'][note]
-			        if len(chords)>1:
-			            chords.pop(random.randint(0,100)%2)
-			        random.shuffle(chords[0])
-			    except KeyError:
-			        chords = None
-		        print ("Note: "+note+" Vel:"+str(vel)+" chords found:"+str(chords))
-
+#    print pygame.midi.get_device_info(3)
+	while not hasStarted:
+		midi_events = i.read(1)
+		if midi_events[0][0][0] == 144 :
+			hasStarted = True
+			bar_start = midi_events[0][1] - time_interval/2
+			next_bar = bar_start + (4*time_interval)
+			next_bar_count = next_bar + (time_interval/2)
 	going = True
     while going:
         if i.poll():
 			midi_events = i.read(1)
 			print(midi_events)
-
-			interStart = start_time + bar_count*(time_interval/2)
-			interEnd = start_time + bar_count*(time_interval*3/2)
-
-			if midi_events[0][0][2] != 0 and (midi_events[1]>interStart or midi_events[1]>interEnd): 			   #velocity!=0 [keyPressed]
+			if midi_events[0][1] < next_bar:
+				pass
+			elif midi_events[0][1] > next_bar and midi_events[0][1] < next_bar_count and midi_events[0][0][0] == 144:
 				note = NOTES[midi_events[0][0][1]%12]		       #noteNum % 12
 			    vel = midi_events[0][0][2]
 			    try:
@@ -80,9 +68,22 @@ def getMidiInput():						#gets midi input, and converts to note
 			        if len(chords)>1:
 			            chords.pop(random.randint(0,100)%2)
 			        random.shuffle(chords[0])
+					prevChord = chords[0]
 			    except KeyError:
-			        chords = None
-		        print ("Note: "+note+" Vel:"+str(vel)+" chords found:"+str(chords))
+			        chords = []]
+				#sa
+				chords = list(map(lambda c : [note2num[c],midi_events[0][0][2]],chords))
+				for i in range(4):	#number of notes in chord
+					queue[i] = chords[i]
+				print ("Note: "+note+" Vel:"+str(vel)+" chords found:"+str(chords))
+			else:
+				chords = list(map(lambda c : [note2num[c],midi_events[0][0][2]],prevChord))
+				for i in range(4):	#number of notes in chord
+					queue[i] = chords[i]
+
+			next_bar = next_bar + (time_interval*4)
+			next_bar_count = next_bar + (time_interval/2)
+
 	pygame.midi.quit()
 
 #------------------OUTPUT--------------------
@@ -91,7 +92,9 @@ def getMidiInput():						#gets midi input, and converts to note
 #     output.note_on(note2num[chordNote], velocity=100)
 #     time.sleep(0.17)
 #     output.note_off(note2num[chordNote], velocity=0)
-
-initTempo()
-populateRules()
-getInput()
+if __name__ == "__main__":
+	import SharedArray as sa
+	queue = sa.create("shm://notes",4)
+	initTempo()
+	populateRules()
+	getMidiInput()
