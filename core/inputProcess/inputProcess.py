28ac60f31cfea734ece5fbd19b110b1d7c4ba29d
Vishalkks <vishalkks@gmail.com>
2017-11-25 21:23:43 +0530

    split for decreasing latency

---
+++ core/inputProcess/inputProcess.py
@@ -0,0 +1,9 @@
+def getNote():
+	#gets midi input, and converts to note
+	pass
+
+def getLHSChord():
+	#takes the note and gives the output chord, does the tempo concept, and puts this string on shared memory
+	pass
+	
+
---
+++ core/output/output.py
@@ -0,0 +1,3 @@
+def sendOutput():
+	#puts the output to the keyboard, gives the midi output back after reading from shared memory
+	pass
